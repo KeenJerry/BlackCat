697afe06cb7fa4fe13d8943bce8edec2f3f19d49
Keen Jackdaw <keenjackdaw@gmail.com>
2018-09-21 11:27:28 +0800

    blackcat init

---
+++ app/src/test/java/com/android/keenjackdaw/blackcat/BlackCatLibTest.java
@@ -0,0 +1,10 @@
+package com.android.keenjackdaw.blackcat;
+
+import com.rokid.citrus.citrusfacesdk.CitrusFaceSDK;
+
+public class BlackCatLibTest {
+    public static void main(String args[]){
+        System.out.println("Start BlackCatLib test...");
+
+    }
+}
