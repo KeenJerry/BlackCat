7a5d7f0371ad03cba8f264c205275fa56ed93c09
Keen Jackdaw <keenjackdaw@gmail.com>
2018-10-17 16:19:14 +0800

    [Add] Add Camera View

---
+++ app/src/main/java/com/android/keenjackdaw/blackcat/ui/CameraView.java
@@ -0,0 +1,21 @@
+package com.android.keenjackdaw.blackcat.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.TextureView;
+
+public class CameraView extends TextureView {
+    public CameraView(Context context) {
+        super(context);
+    }
+
+    public CameraView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CameraView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
