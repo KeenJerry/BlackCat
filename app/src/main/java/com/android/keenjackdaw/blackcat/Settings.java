0db19538f460f22b76905fe100fb6a6d272689f2
Keen Jackdaw <keenjackdaw@gmail.com>
2018-10-20 11:58:33 +0800

    [Update] Complete SDK init func

--- app/src/main/java/com/android/keenjackdaw/blackcat/Settings.java
+++ app/src/main/java/com/android/keenjackdaw/blackcat/Settings.java
@@ -1,7 +1,7 @@
 package com.android.keenjackdaw.blackcat;
 
-public class BaseInfo {
-    private BaseInfo(){}
+public class Settings {
+    private Settings(){}
 
     public final static int standardDensity = 160;
 
@@ -15,4 +15,13 @@ public class BaseInfo {
     public static final String secret = "rokid_test_secret";
     public static final String deviceTypeId = "rokid_test_device_type_id";
     public static final String deviceId = "rokid_test_device_id";
+
+    public static final int imageChannelNum = 3;
+    public static final int frameNumInCache = 3;
+
+    public static final float roiX = 0.0f;
+    public static final float roiY = 0.0f;
+    public static final float roiH = 1.0f;
+    public static final float roiW = 1.0f;
+    public static final float scale = 1.0f;
 }
--- app/src/main/java/com/android/keenjackdaw/blackcat/controller/CitrusFaceManager.java
+++ app/src/main/java/com/android/keenjackdaw/blackcat/controller/CitrusFaceManager.java
@@ -3,19 +3,23 @@ package com.android.keenjackdaw.blackcat.controller;
 import android.content.Context;
 import android.os.Environment;
 
-import com.android.keenjackdaw.blackcat.BaseInfo;
+import com.android.keenjackdaw.blackcat.Settings;
 import com.android.keenjackdaw.blackcat.BlackCatApplication;
+import com.android.keenjackdaw.blackcat.R;
 import com.android.keenjackdaw.blackcat.activity.CameraActivity;
 import com.android.keenjackdaw.blackcat.exception.BlackCatException;
+import com.android.keenjackdaw.blackcat.ui.CameraView;
 import com.rokid.citrus.citrusfacesdk.CitrusFaceSDK;
+
+import java.util.Arrays;
 
 public class CitrusFaceManager {
     private CitrusFaceManager() {}
 
     private static CitrusFaceManager instance = new CitrusFaceManager();
+    private byte[][] byteBuffers = null;
     private CameraActivity cameraActivity = null;
     private Context appContext = null;
-
     private CitrusFaceSDK citrusFaceSDK = null;
 
     public static CitrusFaceManager getInstance() {
@@ -34,31 +38,62 @@ public class CitrusFaceManager {
             throw new BlackCatException("SDK auth failed.");
         }
         citrusFaceSDK.Init(appContext);
-        
+
+        // FIXME It's not an error.
+        CameraView cameraView = cameraActivity.getFragmentContainer().getView().findViewById(R.id.camera_view);
+        setBuffer(cameraView.getWidth(), cameraView.getHeight());
+
+        citrusFaceSDK.SetSizeROIWithBuffer(
+                cameraView.getWidth(),
+                cameraView.getHeight(),
+                Settings.imageChannelNum,
+                Settings.frameNumInCache,
+                byteBuffers,
+                (int)Settings.roiX * cameraView.getWidth(),
+                (int)Settings.roiY * cameraView.getHeight(),
+                (int)Settings.roiW * cameraView.getWidth(),
+                (int)Settings.roiH * cameraView.getHeight(),
+                Settings.scale);
+
         String dbPath = Environment.getExternalStorageDirectory().getPath() + "face.db";
-        BaseInfo.ExternalStorageState state = checkExternalStorage();
-        if(state != BaseInfo.ExternalStorageState.All_ALLOWED){
+        Settings.ExternalStorageState state = checkExternalStorage();
+        if(state != Settings.ExternalStorageState.All_ALLOWED){
             throw new BlackCatException("External storage access not allowed or only allow read.");
         }
         citrusFaceSDK.DBSet(dbPath);
 
     }
 
-    private BaseInfo.ExternalStorageState checkExternalStorage(){
+    private Settings.ExternalStorageState checkExternalStorage(){
         String state = Environment.getExternalStorageState();
 
         switch (state){
             case Environment.MEDIA_MOUNTED:
-                return BaseInfo.ExternalStorageState.All_ALLOWED;
+                return Settings.ExternalStorageState.All_ALLOWED;
             case Environment.MEDIA_MOUNTED_READ_ONLY:
-                return BaseInfo.ExternalStorageState.ONLY_READ;
+                return Settings.ExternalStorageState.ONLY_READ;
             default:
-                return BaseInfo.ExternalStorageState.NEITHER_ALLOWED;
+                return Settings.ExternalStorageState.NEITHER_ALLOWED;
         }
     }
 
     private boolean auth(){
-        return citrusFaceSDK.Authenic(BaseInfo.key, BaseInfo.secret, BaseInfo.deviceId, BaseInfo.deviceTypeId);
+        return citrusFaceSDK.Authenic(Settings.key, Settings.secret, Settings.deviceId, Settings.deviceTypeId);
     }
-    public void startFaceDetection(){}
+
+    private void setBuffer(int width, int height){
+        byteBuffers = new byte[Settings.imageChannelNum][];
+
+        for(int i = 0; i < Settings.imageChannelNum; i++){
+            byteBuffers[i] = new byte[width * height * 3 / 2];
+        }
+        for (int i = 0; i < Settings.imageChannelNum; i++) {
+            Arrays.fill(byteBuffers[i], (byte) 0);
+        }
+    }
+
+    public void startFaceDetection(){
+
+    }
+
 }
