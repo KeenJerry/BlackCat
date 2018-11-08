2d3af0cc19972e18501fec2a606c719be202bc78
Keen Jackdaw <keenjackdaw@gmail.com>
2018-11-08 14:07:21 +0800

    [Update] Update data center activity

--- app/src/main/java/com/android/keenjackdaw/blackcat/activity/DataCenterActivity.java
+++ app/src/main/java/com/android/keenjackdaw/blackcat/activity/DataCenterActivity.java
@@ -1,15 +1,34 @@
 package com.android.keenjackdaw.blackcat.activity;
 
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 import com.android.keenjackdaw.blackcat.R;
+import com.android.keenjackdaw.blackcat.fragment.CameraFragment;
 
 public class DataCenterActivity extends AppCompatActivity {
 
+    Fragment fragmentContainer = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_center);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentContainer = fragmentManager.findFragmentById(R.id.camera_fragment_container);
+        if(fragmentContainer == null){
+            fragmentContainer = new CameraFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, fragmentContainer)
+                    .commit();
+        }
     }
+
+
 }
